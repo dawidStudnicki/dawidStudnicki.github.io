436a2bbe21c3c5c9237864256cdbf2672e204490
dawidStudnicki <dawid.studnicki@rspective.pl>
2021-04-04 21:59:22 +0200

    first commit

---
+++ content/test2.md
@@ -0,0 +1,9 @@
+---
+title: Post Two
+date: “15838264020”
+description: My reasons for starting a blog.
+---
+
+# This is an amazing blog post.
+
+Really it’s just great
---
+++ src/config.js
@@ -0,0 +1,15 @@
+const config = {
+  blogName: 'Rspective Blog',
+  blogDescription: 'Sharing our ideas',
+  authorName: 'Krzysztof Jażyna',
+  authorDescription:
+    'Krzysztof Jażyna urodził się w Szczecinie. Od małego lubiał łapac motyle.',
+  authorTwitter: 'https://twitter.com/chris-vegetable',
+
+  dev: {
+    postsdir: './content',
+    outdir: './public',
+  },
+};
+
+module.exports = config;
